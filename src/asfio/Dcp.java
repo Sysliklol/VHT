6423a80d51fd7d462aa0c594fbfecc548e28a218
Sysliklol <vkharko@gmail.com>
2017-02-20 23:01:48 +0200

    asdasdasdasd

--- src/asfio/Dcp.java
+++ src/asfio/Dcp.java
@@ -2,10 +2,9 @@ package asfio;
 
 public class Dcp {
       public static void main(String[] args){
-<<<<<<< HEAD
-    	  System.out.println("sd123123123123123a");
-=======
-    	  System.out.println("sda123123");
->>>>>>> hello
+
+    	  System.out.println("<3");
+
+
       }
 }
